4cb80d52969b4504a5c0973fcaf871c502fe0c4b
Srivatsan <vatsan1993@gmail.com>
2023-11-03 17:10:30 +0530

    finished program57

---
+++ program57.js
@@ -0,0 +1,29 @@
+// hasSymmetry
+
+// Write a function hasSymmetry(array) that takes in an array. The function should return true if the array has symmetry, false otherwise. For an array to have symmetry, it should be the same forwards and backwards.
+
+// TIP: In JavaScript, it is not possible to compare arrays for equality with ===. In other words, [1, 2, 3] === [1, 2, 3] evaluates to false.
+
+// Examples:
+
+let hasSymmetry = (arr) => {
+  let midIndex = arr.length / 2;
+  let left = arr.slice(0, midIndex);
+  let right = arr.slice(midIndex);
+  for (let val of left) {
+    if (!right.includes(val)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(hasSymmetry([1, 2, 3, 3, 2, 1]));
+console.log(hasSymmetry([1, 2, 3, 3, 4, 1]));
+console.log(hasSymmetry(['cat', 'dog', 'bird', 'dog', 'cat']));
+console.log(hasSymmetry(['cat', 'dog', 'bird', 'bird', 'cat']));
+
+// => true
+// => false
+// => true
+// => false
